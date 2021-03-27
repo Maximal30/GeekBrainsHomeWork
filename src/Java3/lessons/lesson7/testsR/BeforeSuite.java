5c3e2e9a04cbc6a3916d9c900bbf8cb51653b987
maximal30 <maximal3003@gmail.com>
2021-03-27 20:02:16 +0300

    Add_less__7

---
+++ src/Java3/lessons/lesson7/testsR/BeforeSuite.java
@@ -0,0 +1,12 @@
+package Java3.lessons.lesson7.testsR;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BeforeSuite {
+}
+
